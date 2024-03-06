0f8e68338ca56607267c610c4dc9eba04a109742
Aziz <ngwk.zz@outlook.com>
2024-03-06 07:10:27 +0000

    return a promise

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,6 @@
 export function taskFirst() {
   const task = 'I prefer const when I can.';
+
   return task;
 }
 
@@ -9,6 +10,7 @@ export function getLast() {
 
 export function taskNext() {
   let combination = 'But sometimes let';
+
   combination += getLast();
 
   return combination;
---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+  // eslint-disable-next-line no-unused-vars
+    resolve();
+    // eslint-disable-next-line no-unused-vars
+    reject();
+  });
+
+  return promise;
+}
