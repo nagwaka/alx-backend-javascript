8d561f67509fe939eeb02a0eb9bc41695c192247
Aziz <ngwk.zz@outlook.com>
2024-03-06 09:52:35 +0000

    Load balancer

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call signUpUser and uploadPhoto functions with provided arguments
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  // Handle promises using Promise.all
-  return Promise.all([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({ status: 'fulfilled', value: result })))
-    .catch((error) => [{ status: 'rejected', value: error }]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => (
+      response.map((obj) => ({
+        status: obj.status,
+        value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+      }))
+    ));
 }
