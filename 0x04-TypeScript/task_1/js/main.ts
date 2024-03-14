ae06665211fe900796a2dc0b99a7dbc051ca91e0
Aziz <ngwk.zz@outlook.com>
2024-03-14 07:35:08 +0000

    build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // Allow any additional attribute
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false, // Additional attribute
+};
+
+console.log(teacher3);
